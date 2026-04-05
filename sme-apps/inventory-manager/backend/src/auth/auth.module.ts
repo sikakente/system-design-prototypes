d9b41c0626c0f1a70d4782edbce0edadaf6c23b5
Kenneth Mensah <kenneth.mensah220000@gmail.com>
2026-04-06 00:58:33 +0100

    feat(backend): export Auth0ManagementService from AuthModule

--- sme-apps/inventory-manager/backend/src/auth/auth.module.ts
+++ sme-apps/inventory-manager/backend/src/auth/auth.module.ts
@@ -3,10 +3,12 @@ import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { Auth0Strategy } from './auth0.strategy';
+import { Auth0ManagementService } from './auth0-management.service';
 
 @Module({
   imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
   controllers: [AuthController],
-  providers: [AuthService, Auth0Strategy],
+  providers: [AuthService, Auth0Strategy, Auth0ManagementService],
+  exports: [Auth0ManagementService],
 })
 export class AuthModule {}
